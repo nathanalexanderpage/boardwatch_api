d4a8f3a4a19387724d99af93f253373d4f0ecda9
Nathan Page <nathanalexanderpage@gmail.com>
2020-08-20 03:41:59 -0700

    working /platforms/search route

--- api/data_helpers.py
+++ api/data_helpers.py
@@ -21,7 +21,22 @@ def get_all_platforms():
     conn = get_db_connection()
     cur = conn.cursor()
 
-    cur.execute('SELECT p.id as id, p.name as name, p.is_brand_missing as is_brand_missing, pf.id as platform_family_id, pf.name as platform_family_name, p.model_no as model_no, p.storage_capacity as storage_capacity, p.description as description, p.disambiguation as disambiguation, p.relevance as relevance FROM platforms as p JOIN platform_families as pf ON pf.id = p.platform_family_id;')
+    cur.execute("""
+        SELECT
+        p.id as id,
+        p.name as name,
+        p.is_brand_missing as is_brand_missing,
+        pf.id as platform_family_id,
+        pf.name as platform_family_name,
+        p.model_no as model_no,
+        p.storage_capacity as storage_capacity,
+        p.description as description,
+        p.disambiguation as disambiguation,
+        p.relevance as relevance
+        FROM platforms as p
+        JOIN platform_families as pf ON pf.id = p.platform_family_id
+        ORDER BY p.relevance DESC;
+        """)
     platforms = cur.fetchall()
 
     all_platforms = []
@@ -38,10 +53,62 @@ def get_platform_by_id(id):
     conn = get_db_connection()
     cur = conn.cursor()
 
-    cur.execute('SELECT p.id as id, p.name as name, p.is_brand_missing as is_brand_missing, pf.id as platform_family_id, pf.name as platform_family_name, p.model_no as model_no, p.storage_capacity as storage_capacity, p.description as description, p.disambiguation as disambiguation, p.relevance as relevance FROM platforms as p JOIN platform_families as pf ON pf.id = p.platform_family_id WHERE p.id=%s LIMIT 1;', (id,))
+    cur.execute("""
+        SELECT
+        p.id as id,
+        p.name as name,
+        p.is_brand_missing as is_brand_missing,
+        pf.id as platform_family_id,
+        pf.name as platform_family_name,
+        p.model_no as model_no,
+        p.storage_capacity as storage_capacity,
+        p.description as description,
+        p.disambiguation as disambiguation,
+        p.relevance as relevance
+        FROM platforms as p JOIN platform_families as pf ON pf.id = p.platform_family_id
+        WHERE p.id=%s LIMIT 1;
+        """, (id,))
     p = cur.fetchone()
 
     if p == None:
         return None
 
     return Platform(id=p[0], name=p[1], is_brand_missing_from_name=p[2], platform_family_id=p[3], platform_family_name=p[4], model_no=p[5], storage_capacity=p[6], description=p[7], disambiguation=p[8], relevance=p[9])
+
+def get_searched_platforms(q=''):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    like_q = '%' + q + '%'
+    data_dict = {
+        'q': q,
+        'like_q': like_q,
+    }
+
+    cur.execute("""
+        SELECT
+        p.id as id,
+        p.name as name,
+        p.is_brand_missing as is_brand_missing,
+        pf.id as platform_family_id,
+        pf.name as platform_family_name, 
+        p.model_no as model_no,
+        p.storage_capacity as storage_capacity,
+        p.description as description,
+        p.disambiguation as disambiguation,
+        p.relevance as relevance
+        FROM platforms as p JOIN platform_families as pf ON pf.id = p.platform_family_id
+        WHERE p.name ILIKE %(like_q)s OR p.model_no ILIKE %(like_q)s
+        ORDER BY p.relevance DESC;
+        """, data_dict)
+
+    platforms = cur.fetchall()
+
+    all_platforms = []
+
+    for p in platforms:
+        current = Platform(id=p[0], name=p[1], is_brand_missing_from_name=p[2], platform_family_id=p[3], platform_family_name=p[4], model_no=p[5], storage_capacity=p[6], description=p[7], disambiguation=p[8], relevance=p[9])
+
+        all_platforms.append(current)
+        
+    return all_platforms
--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request, Response
 from api import app
 
-from .data_helpers import get_all_platforms, get_platform_by_id, get_platform_results
+from .data_helpers import get_all_platforms, get_platform_by_id, get_searched_platforms
 from .helpers import dictify, dictify_all
 
 
@@ -13,22 +13,24 @@ def index():
 @app.route('/search')
 def search():
     q = request.args.get('q')
+    print(q)
     # perform smaller searches in each category for category-mixed search results
     return Response(response="501 Not Implemented", status=501)
 
 
 @app.route('/platforms/search')
-def search_platforms_route(q):
+def search_platforms_route():
     q = request.args.get('q')
-    platform_results = get_platform_results(q)
-    return platform_results
+    platforms = get_searched_platforms(q)
+    dictified = dictify_all(platforms)
+    return jsonify(dictified)
 
 
 @app.route('/platforms')
 def all_platforms_route():
     platforms = get_all_platforms()
-    dicified = dictify_all(platforms)
-    return jsonify(dicified)
+    dictified = dictify_all(platforms)
+    return jsonify(dictified)
 
 
 @app.route('/platforms/<platform_id>')
